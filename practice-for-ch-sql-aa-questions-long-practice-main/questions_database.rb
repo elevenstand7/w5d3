c1857629fad4ea0adb4c2b506045f60b75d4efbc
Avery Berry <AveryRBerry@gmail.com>
2023-07-26 14:00:03 -0400

    fixed pry starting to test

--- practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
@@ -4,7 +4,7 @@ require 'singleton'
 class QuestionsDatabase < SQLite3::Database
   include Singleton
   def initialize
-    super(questions.db)
+    super('questions.db')
     self.type_translation = true
     self.results_as_hash = true
   end
@@ -29,9 +29,14 @@ class Question
     SQL
     return nil unless question.length > 0
     Question.new(question.first)
-
   end
 
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  attr_accessor :id, :title, :body, :author_id
 end
-
-
