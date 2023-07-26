88f25eeed586778a25c3bef0015fa84cd0e78de7
elevenstand7 <elevenstand7@gmail.com>
2023-07-26 14:33:47 -0400

    working on user class

--- practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
@@ -40,3 +40,49 @@ class Question
 
   attr_accessor :id, :title, :body, :author_id
 end
+
+class User
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| self.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      id = ?
+    SQL
+    return nil unless user.length > 0
+    User.new(user.first)
+  end
+
+  def self.find_by_name(fname, lname)
+    users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      fname = ? AND lname = ?
+    SQL
+    return nil unless users.length > 0
+    users.map{|us| User.new(us)}
+    # User.new(users.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  attr_accessor :id, :fname, :lname
+
+
+end
+
